eaad74e4de500d5836cf3c3ae65403c528af1dd5
Peter Z Ferguson <37026623+Pfergs@users.noreply.github.com>
2019-11-04 15:01:49 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# IronBronco
+pip3 install  flask
+pip3 install flask_bootstrap 
+pip3 install flask_wtf
+pip3 install flask_sqlalchemy
+pip3 install flask_login
+
+for database
+pip install flask_script
+pip install flask_migrate
+pip install psycopg2-binary
+
+python manage.py db init
+python manage.py db migrate
+python manage.py db upgrade
+
+https://medium.com/@dushan14/create-a-web-application-with-python-flask-postgresql-and-deploy-on-heroku-243d548335cc
---
+++ model.py
@@ -0,0 +1,24 @@
+from app import db
+
+class Users(UserMixin, db.Model):
+    __tablename__ = 'Users'
+    name = db.Column(db.String(30))
+    email = db.Column(db.String(40),unique=True)
+    password = db.Column(db.String(80))
+    teamid = db.Column(db.String(20))
+    bio = db.Column(db.String(200))
+    swimming = db.Column(db.Float(3, 2))
+    cycling = db.Column(db.Float(5, 2))
+    running = db.Column(db.Float(4,2))
+    id = db.Column(db.Integer(), primary_key=True)
+
+class Team(UserMixin, db.Model):
+    __tablename__ = 'Team'
+    id = db.Column(db.Integer(), primary_key=True)
+    team = db.Column(db.String(20),unique=True)
+    player1 = db.Column(db.String(30))
+    player2 = db.Column(db.String(30))
+    player3 = db.Column(db.String(30))
+    swimming = db.Column(db.Float(3, 2))
+    cycling = db.Column(db.Float(5, 2))
+    running = db.Column(db.Float(4,2))
